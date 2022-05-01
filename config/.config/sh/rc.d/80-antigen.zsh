8e4e73f4f5aac52f399af051f4e711157a34e460
k5-mot <k5.mot.888@gmail.com>
2022-05-01 21:19:51 +0900

    :gear: Update configs.

--- config/.config/sh/rc.d/80-antigen.zsh
+++ config/.config/sh/rc.d/80-antigen.zsh
@@ -1,3 +1,6 @@
+export ADOTDIR="$HOME/.cache/zsh/antigen"
+export ANTIGEN_LOG="$ADOTDIR/antigen.log"
+
 source $HOME/.config/zsh/antigen.zsh
 
 # Load the oh-my-zsh's library.
@@ -5,16 +8,16 @@ antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
 antigen bundle git
-antigen bundle heroku
+# antigen bundle heroku
 antigen bundle pip
-antigen bundle lein
+# antigen bundle lein
 antigen bundle command-not-found
 
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle zsh-users/zsh-history-substring-search
 antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-autosuggestions
+# antigen bundle zsh-users/zsh-autosuggestions
 
 # Load the theme.
 antigen theme robbyrussell
