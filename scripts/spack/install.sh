df822379262cde05dc2c41f85172e67a941956b3
k5-mot <k5.mot.888+github@gmail.com>
2022-08-09 18:16:11 +0900

    :sparkles: Update

--- scripts/spack/install.sh
+++ scripts/spack/install.sh
@@ -5,11 +5,11 @@
 ## https://github.com/spack/spack
 ##
 
+SPACK_ROOT=$HOME/.local/spack
+
 spack install gcc@8.5.0
 spack load    gcc@8.5.0
-SPACK_ROOT=$HOME/.local/spack
 GCC_ROOT=$(find $SPACK_ROOT/opt/spack -type d -regextype posix-basic -regex ".*linux-.*/gcc-.*/gcc-.*" | head -1)
-
 sed -i -e "2i \
 - compiler:                       \n\
     spec: gcc@8.5.0a              \n\
@@ -25,24 +25,9 @@ sed -i -e "2i \
     environment: {}               \n\
     extra_rpaths: []                 " $HOME/.spack/linux/compilers.yaml
 
-# echo "- compiler:"                       >> $HOME/.spack/linux/compilers.yaml
-# echo "    spec: gcc@8.5.0a"              >> $HOME/.spack/linux/compilers.yaml
-# echo "    paths:"                        >> $HOME/.spack/linux/compilers.yaml
-# echo "      cc:  $GCC_ROOT/bin/gcc"      >> $HOME/.spack/linux/compilers.yaml
-# echo "      cxx: $GCC_ROOT/bin/g++"      >> $HOME/.spack/linux/compilers.yaml
-# echo "      f77: $GCC_ROOT/bin/gfortran" >> $HOME/.spack/linux/compilers.yaml
-# echo "      fc:  $GCC_ROOT/bin/gfortran" >> $HOME/.spack/linux/compilers.yaml
-# echo "    flags: {}"                     >> $HOME/.spack/linux/compilers.yaml
-# echo "    operating_system: centos7"     >> $HOME/.spack/linux/compilers.yaml
-# echo "    target: x86_64"                >> $HOME/.spack/linux/compilers.yaml
-# echo "    modules: []"                   >> $HOME/.spack/linux/compilers.yaml
-# echo "    environment: {}"               >> $HOME/.spack/linux/compilers.yaml
-# echo "    extra_rpaths: []"              >> $HOME/.spack/linux/compilers.yaml
-
 spack install gcc@8.5.0 %gcc@8.5.0a
 spack load    gcc@8.5.0
 GCC_ROOT=$(find $SPACK_ROOT/opt/spack -type d -regextype posix-basic -regex ".*linux-.*/gcc-.*/gcc-.*" | head -1)
-
 sed -i -e "2i\
 - compiler:                       \n\
     spec: gcc@8.5.0               \n\
@@ -58,50 +43,41 @@ sed -i -e "2i\
     environment: {}               \n\
     extra_rpaths: []              \n" $HOME/.spack/linux/compilers.yaml
 
-# echo "- compiler:"                       >> $HOME/.spack/linux/compilers.yaml
-# echo "    spec: gcc@8.5.0"               >> $HOME/.spack/linux/compilers.yaml
-# echo "    paths:"                        >> $HOME/.spack/linux/compilers.yaml
-# echo "      cc:  $GCC_ROOT/bin/gcc"      >> $HOME/.spack/linux/compilers.yaml
-# echo "      cxx: $GCC_ROOT/bin/g++"      >> $HOME/.spack/linux/compilers.yaml
-# echo "      f77: $GCC_ROOT/bin/gfortran" >> $HOME/.spack/linux/compilers.yaml
-# echo "      fc:  $GCC_ROOT/bin/gfortran" >> $HOME/.spack/linux/compilers.yaml
-# echo "    flags: {}"                     >> $HOME/.spack/linux/compilers.yaml
-# echo "    operating_system: centos7"     >> $HOME/.spack/linux/compilers.yaml
-# echo "    target: x86_64"                >> $HOME/.spack/linux/compilers.yaml
-# echo "    modules: []"                   >> $HOME/.spack/linux/compilers.yaml
-# echo "    environment: {}"               >> $HOME/.spack/linux/compilers.yaml
-# echo "    extra_rpaths: []"              >> $HOME/.spack/linux/compilers.yaml
+sed -i -e "2i\
+packages:                 \n\
+  all:                    \n\
+    compiler: [gcc@8.5.0] \n" $HOME/.spack/packages.yaml
 
-spack install zsh      %gcc@8.5.0
-spack install fish     %gcc@8.5.0
-spack install git      %gcc@8.5.0
-spack install curl     %gcc@8.5.0
-spack install tmux     %gcc@8.5.0
-spack install vim      %gcc@8.5.0
+spack install      zsh      %gcc@8.5.0
+spack install      fish     %gcc@8.5.0
+spack install      git      %gcc@8.5.0
+spack install      curl     %gcc@8.5.0
+spack install      tmux     %gcc@8.5.0
+spack install      vim      %gcc@8.5.0
 # spack install neovim   %gcc@8.5.0
-spack install python   %gcc@8.5.0
-spack install ruby     %gcc@8.5.0
-spack install rust     %gcc@8.5.0
-spack install node-js  %gcc@8.5.0
-spack install freeglut %gcc@8.5.0
-spack install glew     %gcc@8.5.0
-spack install glfw     %gcc@8.5.0
-spack install glm      %gcc@8.5.0
-spack install gh       %gcc@8.5.0
+spack install      python   %gcc@8.5.0
+spack install -j 1 ruby     %gcc@8.5.0
+spack install      rust     %gcc@8.5.0
+spack install      node-js  %gcc@8.5.0
+spack install      freeglut %gcc@8.5.0
+spack install      glew     %gcc@8.5.0
+spack install      glfw     %gcc@8.5.0
+spack install      glm      %gcc@8.5.0
+spack install      gh       %gcc@8.5.0
 
-spack load    git
-spack load    curl
-spack load    tmux
-spack load    vim
+spack load         git
+spack load         curl
+spack load         tmux
+spack load         vim
 # spack load    neovim
-spack load    python
-spack load    rust
-spack load    node-js
-spack load    freeglut
-spack load    glew
-spack load    glfw
-spack load    glm
-spack load    gh
+spack load         python
+spack load         rust
+spack load         node-js
+spack load         freeglut
+spack load         glew
+spack load         glfw
+spack load         glm
+spack load         gh
 
 spack find python
 if [ $? ]; then
