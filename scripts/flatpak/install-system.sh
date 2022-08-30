925f5463d75032fc44bb217388c930acb74807a2
k5-mot <k5.mot.888+github@gmail.com>
2022-08-30 14:13:19 +0900

    :sparkles: Update installer via flatpak.

--- scripts/flatpak/install-system.sh
+++ scripts/flatpak/install-system.sh
@@ -7,127 +7,126 @@
 ##
 
 ## Remove
-flatpak uninstall --assumeyes com.github.tchx84.Flatseal
-flatpak uninstall --assumeyes org.gnu.emacs
-flatpak uninstall --assumeyes org.vim.Vim
-flatpak uninstall --assumeyes io.neovim.nvim
-flatpak uninstall --assumeyes com.visualstudio.code
-flatpak uninstall --assumeyes org.apache.netbeans
-flatpak uninstall --assumeyes org.eclipse.Java
-flatpak uninstall --assumeyes com.jetbrains.PyCharm-Community
-flatpak uninstall --assumeyes com.unity.UnityHub
-flatpak uninstall --assumeyes io.github.shiftey.Desktop
-flatpak uninstall --assumeyes com.axosoft.GitKraken
-flatpak uninstall --assumeyes com.raggesilver.BlackBox
-flatpak uninstall --assumeyes org.blender.Blender
-flatpak uninstall --assumeyes org.freecadweb.FreeCAD
-flatpak uninstall --assumeyes org.kicad.KiCad
-flatpak uninstall --assumeyes org.gimp.GIMP
-flatpak uninstall --assumeyes org.inkscape.Inkscape
-flatpak uninstall --assumeyes org.octave.Octave
-flatpak uninstall --assumeyes com.github.PintaProject.Pinta
-flatpak uninstall --assumeyes org.kde.krita
-flatpak uninstall --assumeyes org.audacityteam.Audacity
-flatpak uninstall --assumeyes org.kde.kdenlive
-flatpak uninstall --assumeyes org.libreoffice.LibreOffice
-flatpak uninstall --assumeyes com.obsproject.Studio
-flatpak uninstall --assumeyes org.kde.okular
-flatpak uninstall --assumeyes com.google.Chrome
-flatpak uninstall --assumeyes com.google.ChromeDev
-flatpak uninstall --assumeyes com.microsoft.Edge
-flatpak uninstall --assumeyes org.mozilla.firefox
-flatpak uninstall --assumeyes com.brave.Browser
-flatpak uninstall --assumeyes org.chromium.Chromium
-flatpak uninstall --assumeyes org.gnome.Epiphany
-flatpak uninstall --assumeyes com.github.micahflee.torbrowser-launcher
-flatpak uninstall --assumeyes dev.vieb.Vieb
-flatpak uninstall --assumeyes org.mozilla.Thunderbird
-flatpak uninstall --assumeyes org.gnome.Geary
-flatpak uninstall --assumeyes com.slack.Slack
-flatpak uninstall --assumeyes com.discordapp.Discord
-flatpak uninstall --assumeyes us.zoom.Zoom
-flatpak uninstall --assumeyes com.microsoft.Teams
-flatpak uninstall --assumeyes org.videolan.VLC
-flatpak uninstall --assumeyes com.spotify.Client
-flatpak uninstall --assumeyes com.mojang.Minecraft
-flatpak uninstall --assumeyes md.obsidian.Obsidian
-flatpak uninstall --assumeyes com.github.k4zmu2a.spacecadetpinball
-flatpak uninstall --assumeyes org.kde.kmines
-flatpak uninstall --assumeyes org.processing.processingide
-flatpak uninstall --assumeyes org.gabmus.hydrapaper
-flatpak uninstall --assumeyes com.valvesoftware.Steam
-flatpak uninstall --assumeyes org.libreoffice.LibreOffice
-flatpak uninstall --assumeyes io.gitlab.librewolf-community
-flatpak uninstall --assumeyes org.contourterminal.Contour
-flatpak uninstall --assumeyes org.wezfurlong.wezterm
-flatpak uninstall --assumeyes com.github.corna.Vivado
-flatpak uninstall --assumeyes com.google.AndroidStudio
-flatpak uninstall --assumeyes com.sublimetext.three
-flatpak uninstall --assumeyes io.atom.Atom
-flatpak uninstall --assumeyes io.brackets.Brackets
-flatpak uninstall --assumeyes io.qt.QtCreator
-flatpak uninstall --assumeyes com.agateau.PixelWheels
-flatpak uninstall --assumeyes com.anydesk.Anydesk
-flatpak uninstall --assumeyes com.ktechpit.wonderwall
-flatpak uninstall --assumeyes dev.boxi.Boxi
-flatpak uninstall --assumeyes io.github.f3d_app.f3d
-flatpak uninstall --assumeyes io.github.shiiion.primehack
-flatpak uninstall --assumeyes net.devolutions.RDM
-flatpak uninstall --assumeyes net.meshlab.MeshLab
-flatpak uninstall --assumeyes org.geogebra.GeoGebra
-flatpak uninstall --assumeyes org.kde.krdc
+sudo flatpak uninstall --assumeyes com.github.tchx84.Flatseal
+sudo flatpak uninstall --assumeyes org.gnu.emacs
+sudo flatpak uninstall --assumeyes org.vim.Vim
+sudo flatpak uninstall --assumeyes io.neovim.nvim
+sudo flatpak uninstall --assumeyes com.visualstudio.code
+sudo flatpak uninstall --assumeyes org.apache.netbeans
+sudo flatpak uninstall --assumeyes org.eclipse.Java
+sudo flatpak uninstall --assumeyes com.jetbrains.PyCharm-Community
+sudo flatpak uninstall --assumeyes com.unity.UnityHub
+sudo flatpak uninstall --assumeyes io.github.shiftey.Desktop
+sudo flatpak uninstall --assumeyes com.axosoft.GitKraken
+sudo flatpak uninstall --assumeyes com.raggesilver.BlackBox
+sudo flatpak uninstall --assumeyes org.blender.Blender
+sudo flatpak uninstall --assumeyes org.freecadweb.FreeCAD
+sudo flatpak uninstall --assumeyes org.kicad.KiCad
+sudo flatpak uninstall --assumeyes org.gimp.GIMP
+sudo flatpak uninstall --assumeyes org.inkscape.Inkscape
+sudo flatpak uninstall --assumeyes org.octave.Octave
+sudo flatpak uninstall --assumeyes com.github.PintaProject.Pinta
+sudo flatpak uninstall --assumeyes org.kde.krita
+sudo flatpak uninstall --assumeyes org.audacityteam.Audacity
+sudo flatpak uninstall --assumeyes org.kde.kdenlive
+sudo flatpak uninstall --assumeyes org.libreoffice.LibreOffice
+sudo flatpak uninstall --assumeyes com.obsproject.Studio
+sudo flatpak uninstall --assumeyes org.kde.okular
+sudo flatpak uninstall --assumeyes com.google.Chrome
+sudo flatpak uninstall --assumeyes com.google.ChromeDev
+sudo flatpak uninstall --assumeyes com.microsoft.Edge
+sudo flatpak uninstall --assumeyes org.mozilla.firefox
+sudo flatpak uninstall --assumeyes com.brave.Browser
+sudo flatpak uninstall --assumeyes org.chromium.Chromium
+sudo flatpak uninstall --assumeyes org.gnome.Epiphany
+sudo flatpak uninstall --assumeyes com.github.micahflee.torbrowser-launcher
+sudo flatpak uninstall --assumeyes dev.vieb.Vieb
+sudo flatpak uninstall --assumeyes org.mozilla.Thunderbird
+sudo flatpak uninstall --assumeyes org.gnome.Geary
+sudo flatpak uninstall --assumeyes com.slack.Slack
+sudo flatpak uninstall --assumeyes com.discordapp.Discord
+sudo flatpak uninstall --assumeyes us.zoom.Zoom
+sudo flatpak uninstall --assumeyes com.microsoft.Teams
+sudo flatpak uninstall --assumeyes org.videolan.VLC
+sudo flatpak uninstall --assumeyes com.spotify.Client
+sudo flatpak uninstall --assumeyes com.mojang.Minecraft
+sudo flatpak uninstall --assumeyes md.obsidian.Obsidian
+sudo flatpak uninstall --assumeyes com.github.k4zmu2a.spacecadetpinball
+sudo flatpak uninstall --assumeyes org.kde.kmines
+sudo flatpak uninstall --assumeyes org.processing.processingide
+sudo flatpak uninstall --assumeyes org.gabmus.hydrapaper
+sudo flatpak uninstall --assumeyes com.valvesoftware.Steam
+sudo flatpak uninstall --assumeyes org.libreoffice.LibreOffice
+sudo flatpak uninstall --assumeyes io.gitlab.librewolf-community
+sudo flatpak uninstall --assumeyes org.contourterminal.Contour
+sudo flatpak uninstall --assumeyes org.wezfurlong.wezterm
+sudo flatpak uninstall --assumeyes com.github.corna.Vivado
+sudo flatpak uninstall --assumeyes com.google.AndroidStudio
+sudo flatpak uninstall --assumeyes com.sublimetext.three
+sudo flatpak uninstall --assumeyes io.atom.Atom
+sudo flatpak uninstall --assumeyes io.brackets.Brackets
+sudo flatpak uninstall --assumeyes io.qt.QtCreator
+sudo flatpak uninstall --assumeyes com.agateau.PixelWheels
+sudo flatpak uninstall --assumeyes com.anydesk.Anydesk
+sudo flatpak uninstall --assumeyes com.ktechpit.wonderwall
+sudo flatpak uninstall --assumeyes dev.boxi.Boxi
+sudo flatpak uninstall --assumeyes io.github.f3d_app.f3d
+sudo flatpak uninstall --assumeyes io.github.shiiion.primehack
+sudo flatpak uninstall --assumeyes net.devolutions.RDM
+sudo flatpak uninstall --assumeyes net.meshlab.MeshLab
+sudo flatpak uninstall --assumeyes org.geogebra.GeoGebra
+sudo flatpak uninstall --assumeyes org.kde.krdc
 
-flatpak uninstall --assumeyes --unused
+sudo flatpak uninstall --assumeyes --unused
 
-flatpak remote-delete flathub
-flatpak remote-delete flathub-beta
-flatpak remote-delete elementary
-flatpak remote-delete kdeapps
-flatpak remotes
+sudo flatpak remote-delete flathub
+sudo flatpak remote-delete flathub-beta
+sudo flatpak remote-delete elementary
+sudo flatpak remote-delete kdeapps
+sudo flatpak remotes
 
 ## Setup
-export GPG_TTY=$(tty)
-flatpak remote-add --if-not-exists flathub      https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
-flatpak remote-add --if-not-exists elementary   https://flatpak.elementary.io/repo.flatpakrepo
-flatpak remote-add --if-not-exists kdeapps      https://distribute.kde.org/kdeapps.flatpakrepo
-flatpak remotes
+sudo flatpak remote-add --if-not-exists flathub      https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+sudo flatpak remote-add --if-not-exists elementary   https://flatpak.elementary.io/repo.flatpakrepo
+sudo flatpak remote-add --if-not-exists kdeapps      https://distribute.kde.org/kdeapps.flatpakrepo
+sudo flatpak remotes
 
 
 ## Update
-flatpak repair --assumeyes
-flatpak update --assumeyes
+sudo flatpak repair --assumeyes
+sudo flatpak update --assumeyes
 
 ## Install
-flatpak install --assumeyes flathub com.github.tchx84.Flatseal
-flatpak install --assumeyes flathub io.neovim.nvim
-flatpak install --assumeyes flathub com.visualstudio.code
+sudo flatpak install --assumeyes flathub com.github.tchx84.Flatseal
+sudo flatpak install --assumeyes flathub io.neovim.nvim
+sudo flatpak install --assumeyes flathub com.visualstudio.code
 # flatpak install --assumeyes flathub org.eclipse.Java
-flatpak install --assumeyes flathub io.github.shiftey.Desktop
-flatpak install --assumeyes flathub org.blender.Blender
-flatpak install --assumeyes flathub org.freecadweb.FreeCAD
-flatpak install --assumeyes flathub org.kicad.KiCad
-flatpak install --assumeyes flathub org.gimp.GIMP
-flatpak install --assumeyes flathub org.inkscape.Inkscape
-flatpak install --assumeyes flathub org.octave.Octave
-flatpak install --assumeyes flathub org.libreoffice.LibreOffice
+sudo flatpak install --assumeyes flathub io.github.shiftey.Desktop
+sudo flatpak install --assumeyes flathub org.blender.Blender
+sudo flatpak install --assumeyes flathub org.freecadweb.FreeCAD
+sudo flatpak install --assumeyes flathub org.kicad.KiCad
+sudo flatpak install --assumeyes flathub org.gimp.GIMP
+sudo flatpak install --assumeyes flathub org.inkscape.Inkscape
+sudo flatpak install --assumeyes flathub org.octave.Octave
+sudo flatpak install --assumeyes flathub org.libreoffice.LibreOffice
 # flatpak install --assumeyes flathub org.kde.okular
-flatpak install --assumeyes flathub com.google.Chrome
-flatpak install --assumeyes flathub org.mozilla.firefox
-flatpak install --assumeyes flathub org.mozilla.Thunderbird
-flatpak install --assumeyes flathub com.microsoft.Edge
-flatpak install --assumeyes flathub com.discordapp.Discord
-flatpak install --assumeyes flathub com.slack.Slack
-flatpak install --assumeyes flathub us.zoom.Zoom
-flatpak install --assumeyes flathub com.microsoft.Teams
-flatpak install --assumeyes flathub org.videolan.VLC
+sudo flatpak install --assumeyes flathub com.google.Chrome
+sudo flatpak install --assumeyes flathub org.mozilla.firefox
+sudo flatpak install --assumeyes flathub org.mozilla.Thunderbird
+sudo flatpak install --assumeyes flathub com.microsoft.Edge
+sudo flatpak install --assumeyes flathub com.discordapp.Discord
+sudo flatpak install --assumeyes flathub com.slack.Slack
+sudo flatpak install --assumeyes flathub us.zoom.Zoom
+sudo flatpak install --assumeyes flathub com.microsoft.Teams
+sudo flatpak install --assumeyes flathub org.videolan.VLC
 # flatpak install --assumeyes flathub com.spotify.Client
 # flatpak install --assumeyes flathub com.mojang.Minecraft
 # flatpak install --assumeyes flathub com.github.k4zmu2a.spacecadetpinball
 # flatpak install --assumeyes flathub org.kde.kmines
 # flatpak install --assumeyes flathub org.processing.processingide
-flatpak install --assumeyes flathub org.wezfurlong.wezterm
-flatpak install --assumeyes flathub io.github.f3d_app.f3d
+sudo flatpak install --assumeyes flathub org.wezfurlong.wezterm
+sudo flatpak install --assumeyes flathub io.github.f3d_app.f3d
 # flatpak install --assumeyes flathub com.google.ChromeDev
 # flatpak install --assumeyes flathub net.meshlab.MeshLab
 # flatpak install --user --assumeyes flathub org.gnu.emacs
