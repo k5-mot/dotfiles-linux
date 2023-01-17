9418530f6f89824658bc7a6b44c391219012c6c5
k5-mot <k5.mot.888@gmail.com>
2023-01-17 20:14:33 +0900

    A

--- config/.config/sh/profile.d/50-porg.sh
+++ config/.config/sh/profile.d/50-porg.sh
@@ -7,17 +7,17 @@ export HOSTNAME=$(hostname -s)
 export PORG_ROOT=$HOME/.local/porg/$HOSTNAME
 if [ -e $PORG_ROOT ]; then
   export PATH=$PORG_ROOT/usr/bin:$PATH
-  export LD_LIBRARY_PATH=$PORG_ROOT/usr/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$PORG_ROOT/usr/lib64:$PORG_ROOT/usr/lib:$LD_LIBRARY_PATH
   export MANPATH=$PORG_ROOT/share/man:$MANPATH
   # export PATH=$PORG_ROOT/opt/bin:$PORG_ROOT/usr/bin:$PATH
   # export LD_LIBRARY_PATH=$PORG_ROOT/usr/lib64:$PORG_ROOT/usr/lib:$LD_LIBRARY_PATH
-  # export LIBRARY_PATH=$PORG_ROOT/usr/lib64:$PORG_ROOT/usr/lib:$LIBRARY_PATH
+  export LIBRARY_PATH=$PORG_ROOT/usr/lib64:$PORG_ROOT/usr/lib:$LIBRARY_PATH
   # export PKG_CONFIG_PATH=$PORG_ROOT/usr/lib64/pkgconfig:$PORG_ROOT/usr/lib/pkgconfig:$PKG_CONFIG_PATH
   # export MANPATH=$PORG_ROOT/usr/share/man:$MANPATH
   # export INFOPATH=$PORG_ROOT/usr/share/info:$INFOPATH
-  # export CPATH=$PORG_ROOT/usr/include:$CPATH
-  # export C_INCLUDE_PATH=$PORG_ROOT/usr/include:$C_INCLUDE_PATH
-  # export CPLUS_INCLUDE_PATH=$PORG_ROOT/usr/include:$CPLUS_INCLUDE_PATH
+  export CPATH=/usr/include:$PORG_ROOT/usr/include:$CPATH
+  export C_INCLUDE_PATH=/usr/include:$PORG_ROOT/usr/include:$C_INCLUDE_PATH
+  export CPLUS_INCLUDE_PATH=/usr/include:$PORG_ROOT/usr/include:$CPLUS_INCLUDE_PATH
 
   # dirs=("$PORG_ROOT/include/c++" "$PORG_ROOT/usr/include/c++" "$PORG_ROOT/opt/include/c++")
   # for dir1 in $dirs; do
