1ba485645bb3327b938dcc4a0575b2bb12e2c56b
k5-mot <k5.mot.888@gmail.com>
2023-08-08 09:28:45 +0900

    A

--- config/.config/sh/rc.d/80-zinit.zsh
+++ config/.config/sh/rc.d/80-zinit.zsh
@@ -43,9 +43,8 @@ if [ "$ZSH_VERSION" -ge 50400 ]; then
     # zinit light zsh-users/zsh-syntax-highlighting
     # zinit ice wait lucid
     zinit light chrissicool/zsh-256color
-    zinit light zsh-users/zsh-syntax-highlighting
-    # zinit light zdharma-continuum/fast-syntax-highlighting
-    # zinit light zdharma-continuum/fast-syntax-highlighting
+    # zinit light zsh-users/zsh-syntax-highlighting
+    zinit light zdharma-continuum/fast-syntax-highlighting
     zinit light zdharma-continuum/history-search-multi-word
 
     # zinit ice pick"async.zsh" src"pure.zsh" # with zsh-async library that's bundled with it.
